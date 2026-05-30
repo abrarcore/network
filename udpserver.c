5b2ea384c53fc41a8430a27a6dcac9fc81215d5a
abrarcore <trustline47@gmail.com>
2026-05-30 18:19:25 +0530

    Update udpserver.c

--- udpserver.c
+++ udpserver.c
@@ -9,10 +9,9 @@ int main()
 {
     int s_sd;
     socklen_t serv_len;
-    char sour[200], dest[200];
+    char a[200], b[200];
 
     struct sockaddr_in serv, cli;
-
     serv_len = sizeof(cli);
 
     s_sd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -20,23 +19,16 @@ int main()
     serv.sin_family = AF_INET;
     serv.sin_port = htons(8002);
     serv.sin_addr.s_addr = inet_addr("127.0.0.1");
-
     bind(s_sd, (struct sockaddr *)&serv, sizeof(serv));
-
     printf("\nThe SERVER is WAITING...\n");
 
     while (1)
     {
-        recvfrom(s_sd, sour, sizeof(sour), 0,
-                 (struct sockaddr *)&cli, &serv_len);
-
-        printf("\nMessage from client: %s\n", sour);
-
+        recvfrom(s_sd, a, sizeof(a), 0, (struct sockaddr *)&cli, &serv_len);
+        printf("\nMessage from client: %s\n", a);
         printf("Enter message for client: ");
-        scanf(" %[^\n]", dest);
-
-        sendto(s_sd, dest, strlen(dest) + 1, 0,
-               (struct sockaddr *)&cli, serv_len);
+        scanf(" %[^\n]", b);
+        sendto(s_sd, b, strlen(b) + 1, 0,  (struct sockaddr *)&cli, serv_len);
     }
 
     close(s_sd);
