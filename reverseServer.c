a152b9475e2484475d74a48abd404e2cd53f4832
abrarcore <trustline47@gmail.com>
2026-05-30 18:13:59 +0530

    Update reverseServer.c

--- reverseServer.c
+++ reverseServer.c
@@ -6,61 +6,53 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-#define MAXLINE 20
-#define SERV_PORT 5777
 
 int main(int argc, char *argv[])
 {
     int i, j;
     ssize_t n;
 
-    char line[MAXLINE], revline[MAXLINE];
+    char a[50], b[50];
 
-    int listenfd, connfd, clilen;
+    int sockfd, clifd, clilen;
 
     struct sockaddr_in servaddr, cliaddr;
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&servaddr, sizeof(servaddr));
 
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(SERV_PORT);
+    servaddr.sin_port = htons(5777);
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    bind(listenfd,
-         (struct sockaddr *)&servaddr,
-         sizeof(servaddr));
-
-    listen(listenfd, 7);
+    bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    listen(sockfd, 7);
 
     for (;;)
     {
         clilen = sizeof(cliaddr);
 
-        connfd = accept(listenfd,
-                        (struct sockaddr *)&cliaddr,
-                        &clilen);
-
+        clifd = accept(sockfd, (struct sockaddr *)&cliaddr, &clilen);
         printf("Connected to client\n");
 
         while (1)
         {
-            if ((n = read(connfd, line, MAXLINE)) == 0)
+            if ((n = read(clifd, a, MAXa)) == 0)
                 break;
 
-            line[n] = '\0';
+            a[n] = '\0';
 
             j = 0;
 
             for (i = n - 1; i >= 0; i--)
             {
-                revline[j++] = line[i];
+                b[j++] = a[i];
             }
 
-            revline[j] = '\0';
+            b[j] = '\0';
 
-            write(connfd, revline, n);
+            write(clifd, b, n);
         }
     }
 
