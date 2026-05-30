be9784712a9b6d867089c1e88a7135b1a4c68760
abrarcore <trustline47@gmail.com>
2026-05-30 18:13:11 +0530

    Update tcpServer.c

--- tcpServer.c
+++ tcpServer.c
@@ -11,45 +11,40 @@ int main()
 {
     int serv_sockfd, cli_sockfd;
     int serv_len, cli_len;
-
+    // address type
     struct sockaddr_in serv_address, cli_addr;
-
+    // create msg storage
     char a[100], b[100];
-
+    // create socket
     serv_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // configure
 
     serv_address.sin_family = AF_INET;
     serv_address.sin_port = htons(9001);
     serv_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+
     serv_len = sizeof(serv_address);
+    //bind server and socket
+    bind(serv_sockfd, (struct sockaddr *)&serv_address, serv_len);
 
-    bind(serv_sockfd,
-         (struct sockaddr *)&serv_address,
-         serv_len);
-
+    //listen fromclient
     listen(serv_sockfd, 5);
-
     printf("Server waiting...\n");
-
     cli_len = sizeof(cli_addr);
 
-    cli_sockfd = accept(serv_sockfd,
-                        (struct sockaddr *)&cli_addr,
-                        &cli_len);
+    // accepts request from client
+    cli_sockfd = accept(serv_sockfd, (struct sockaddr *)&cli_addr, &cli_len);
 
     printf("Client connected.\n");
 
     while(1)
     {
         read(cli_sockfd, a, sizeof(a));
-
         printf("Client: %s\n", a);
-
-
         printf("Server: ");
         gets(b);
-
         write(cli_sockfd, b, sizeof(b));
     }
 
